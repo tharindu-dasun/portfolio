ca70f54246591d525070c594f8795f05e77c0053
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-08-30 13:12:16 +0530

    update 0.3

--- lk.ijse.gdse/Assests/javaScript/style-switcher.js
+++ lk.ijse.gdse/Assests/javaScript/style-switcher.js
@@ -1,10 +1,10 @@
 //=======================toggle style switcher========================
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () =>{
+styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
 // hide style-switcher on scroll
-window.addEventListener("scroll", () =>{
+window.addEventListener("scroll", () => {
     if (document.querySelector(".style-switcher").classList.contains("open"))
     {
         document.querySelector(".style-switcher").classList.remove("open");
